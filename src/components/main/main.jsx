90188010a97f6d3e49218a0fa1a4e45e2e34b500
laxma.chelimala <@arcadis.com>
2025-10-30 16:10:27 +0530

    changes to tab the element in ui

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -24,121 +24,121 @@ const main = () => {
             aria-label="Explore Microsoft 365"
             className="content rounded-tl-xl overflow-y-auto pb-16 h-lvh bg-gray shadow-[0_0_0_1px_rgba(0,0,0,0.05)]">
             <div aria-label="notification"
-                className="rounded-tl-xl flex items-center bg-gradient-to-r from-red-100 to-blue-100 py-4 px-4 text-sm">
-                <img src={microsoftoffice} className="ml-auto  h-6 mr-2 " alt="microsoftoffice" />
+                className="flex items-center px-4 py-4 text-sm rounded-tl-xl bg-gradient-to-r from-red-100 to-blue-100">
+                <img src={microsoftoffice} className="h-6 ml-auto mr-2 " alt="microsoftoffice" />
 
                 <FeatherIcon icon="arrow-right" strokeWidth="1" className="mr-2" />
                 <img src={micro365} className="h-6 mr-2" alt="micro365" />
                 <span>
                     The Office app is becoming the new Microsoft 365 app. Your home to
                     find, create, and share your content and ideas.
-                    <span className="text-red-800  cursor-pointer">Learn More</span>
+                    <span className="text-red-800 cursor-pointer">Learn More</span>
                 </span>
                 <button className="ml-auto " aria-label="clear notification" >
                     <FeatherIcon icon="x" strokeWidth="1" className="cursor-pointer" />
                 </button>
             </div>
-            <div className="mt-12 px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20" role-="contentinfo" aria-label="content">
+            <div className="px-4 mt-12 sm:px-8 md:px-12 lg:px-16 xl:px-20" role-="contentinfo" aria-label="content">
                 <div aria-labelledby="apps" className="flex items-center justify-between p-2">
                     <h2 id="apps" className="text-xl font-semibold">Apps</h2>
                     <button
                         aria-label="Add apps"
-                        className="flex items-center bg-red-800 text-white px-3 py-1 rounded-md text-sm hover:bg-red-700">
+                        className="flex items-center px-3 py-1 text-sm text-white bg-red-800 rounded-md hover:bg-red-700">
                         <FeatherIcon icon='plus-square' className='mr-1' /> Add apps
                     </button>
                 </div>
-                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 mt-6" aria-labelledby="apps">
-                    <div
-                        className="bg-white shadow p-4 rounded-lg flex flex-col items-center hover:shadow-lg transition">
-                        <img src={teams} className="h-12" alt="teams" />
+                <div className="grid grid-cols-2 gap-4 mt-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6" aria-labelledby="apps">
+                    <button
+                        className="flex flex-col items-center p-4 transition bg-white rounded-lg shadow cursor-pointer hover:shadow-lg">
+                        <img src={teams} className="h-12" alt="teamslogo" />
                         <p className="mt-2 text-sm font-medium">Teams</p>
-                    </div>
-                    <div
-                        className="bg-white shadow p-4 rounded-lg flex flex-col items-center hover:shadow-lg transition">
+                    </button>
+                    <button
+                        className="flex flex-col items-center p-4 transition bg-white rounded-lg shadow cursor-pointer hover:shadow-lg">
                         <img src=
-                            {word} className="h-12" alt="word" />
+                            {word} className="h-12" alt="wordlogo" />
                         <p className="mt-2 text-sm font-medium">Word</p>
-                    </div>
-                    <div
-                        className="bg-white shadow p-4 rounded-lg flex flex-col items-center hover:shadow-lg transition">
-                        <img src={excel} className="h-12" alt="excel" />
+                    </button>
+                    <button
+                        className="flex flex-col items-center p-4 transition bg-white rounded-lg shadow cursor-pointer hover:shadow-lg">
+                        <img src={excel} className="h-12" alt="excellogo" />
                         <p className="mt-2 text-sm font-medium">Excel</p>
-                    </div>
-                    <div
-                        className="bg-white shadow p-4 rounded-lg flex flex-col items-center hover:shadow-lg transition">
-                        <img src={powerpoint} className="h-12" alt="powerpoint" />
+                    </button>
+                    <button
+                        className="flex flex-col items-center p-4 transition bg-white rounded-lg shadow cursor-pointer hover:shadow-lg">
+                        <img src={powerpoint} className="h-12" alt="powerpointlogo" />
                         <p className="mt-2 text-sm font-medium">PowerPoint</p>
-                    </div>
-                    <div
-                        className="bg-white shadow p-4 rounded-lg flex flex-col items-center hover:shadow-lg transition">
-                        <img src={outlook} className="h-12" alt="outlook" />
+                    </button>
+                    <button
+                        className="flex flex-col items-center p-4 transition bg-white rounded-lg shadow cursor-pointer hover:shadow-lg">
+                        <img src={outlook} className="h-12" alt="outlooklogo" />
                         <p className="mt-2 text-sm font-medium">Outlook</p>
-                    </div>
-                    <div
-                        className="bg-white shadow p-4 rounded-lg flex flex-col items-center hover:shadow-lg transition">
-                        <img src={onedrive} className="h-12" alt="onedrive" />
+                    </button>
+                    <button
+                        className="flex flex-col items-center p-4 transition bg-white rounded-lg shadow cursor-pointer hover:shadow-lg">
+                        <img src={onedrive} className="h-12" alt="onedrivelogo" />
                         <p className="mt-2 text-sm font-medium">OneDrive</p>
-                    </div>
-                    <div
-                        className="bg-white shadow p-4 rounded-lg flex flex-col items-center hover:shadow-lg transition">
-                        <img src={onenote} className="h-12" alt="onenote" />
+                    </button>
+                    <button
+                        className="flex flex-col items-center p-4 transition bg-white rounded-lg shadow cursor-pointer hover:shadow-lg">
+                        <img src={onenote} className="h-12" alt="onenotelogo" />
                         <p className="mt-2 text-sm font-medium">OneNote</p>
-                    </div>
+                    </button>
                 </div>
 
                 <div className="text-right">
-                    <button className="text-red-800 hover:underline text-sm">
+                    <button className="text-sm text-red-800 hover:underline">
                         All apps →
                     </button>
                 </div>
                 <div role="region" aria-label="previous added apps">
-                    <h3 id="previouslyadded" className="text-lg font-semibold mt-4 mb-3">Previously added</h3>
-                    <div className="grid md:grid-cols-3 gap-3" aria-label="previous added apps">
-                        <div className="flex items-center  p-3 rounded-md hover:shadow-lg transition cursor-pointer">
+                    <h3 id="previouslyadded" className="mt-4 mb-3 text-lg font-semibold">Previously added</h3>
+                    <div className="grid gap-3 md:grid-cols-3" aria-label="previous added apps">
+                        <button className="flex items-center p-3 transition rounded-md cursor-pointer hover:shadow-lg">
                             <img src={appstudio} alt="appstudio" className="h-8 mr-3" />
                             <p>App Studio</p>
-                        </div>
-                        <div className="flex items-center  p-3 rounded-md hover:shadow-lg transition cursor-pointer">
+                        </button>
+                        <button className="flex items-center p-3 transition rounded-md cursor-pointer hover:shadow-lg">
                             <img src={assignmenticon} className="h-8 mr-3" alt="assignmenticon" />
                             <p>Assignments</p>
-                        </div>
-                        <div className="flex items-center  p-3 rounded-md hover:shadow-lg transition cursor-pointer">
+                        </button>
+                        <button className="flex items-center p-3 transition rounded-md cursor-pointer hover:shadow-lg">
                             <img src={excelfull} className="h-8 mr-3" alt="excel" />
                             <p>Excel</p>
-                        </div>
-                        <div className="flex items-center  p-3 rounded-md hover:shadow-lg transition cursor-pointer">
+                        </button>
+                        <button className="flex items-center p-3 transition rounded-md cursor-pointer hover:shadow-lg">
                             <img src={powerbi} className="h-8 mr-3" alt="powerbi" />
                             <p>Power BI</p>
-                        </div>
-                        <div className="flex items-center  p-3 rounded-md hover:shadow-lg transition cursor-pointer">
+                        </button>
+                        <button className="flex items-center p-3 transition rounded-md cursor-pointer hover:shadow-lg">
                             <img src={powerpointfull} className="h-8 mr-3" alt="powerpoint" />
                             <p>PowerPoint</p>
-                        </div>
-                        <div className="flex items-center  p-3 rounded-md hover:shadow-lg transition cursor-pointer">
+                        </button>
+                        <button className="flex items-center p-3 transition rounded-md cursor-pointer hover:shadow-lg">
                             <img src={taskmodule} className="h-8 mr-3" alt="taskmodule" />
                             <p>Task Module Sample</p>
-                        </div>
-                        <div className="flex items-center  p-3 rounded-md hover:shadow-lg transition cursor-pointer">
+                        </button>
+                        <button className="flex items-center p-3 transition rounded-md cursor-pointer hover:shadow-lg">
                             <img src={wordfull} className="h-8 mr-3" alt="word" />
                             <p>Word</p>
-                        </div>
+                        </button>
                     </div>
                 </div>
                 <div aria-label="categories" className="flex flex-wrap gap-2 mt-6">
                     <button
-                        className="border border-gray-400 rounded-full px-4 py-1 text-sm hover:bg-black hover:text-white transition">
+                        className="px-4 py-1 text-sm transition border border-gray-400 rounded-full hover:bg-black hover:text-white">
                         Productivity
                     </button>
                     <button
-                        className="border border-gray-400 rounded-full px-4 py-1 text-sm hover:bg-black hover:text-white transition">
+                        className="px-4 py-1 text-sm transition border border-gray-400 rounded-full hover:bg-black hover:text-white">
                         Utilities
                     </button>
                     <button
-                        className="border border-gray-400 rounded-full px-4 py-1 text-sm hover:bg-black hover:text-white transition">
+                        className="px-4 py-1 text-sm transition border border-gray-400 rounded-full hover:bg-black hover:text-white">
                         Education
                     </button>
                     <button
-                        className="border border-gray-400 rounded-full px-4 py-1 text-sm hover:bg-black hover:text-white transition">
+                        className="px-4 py-1 text-sm transition border border-gray-400 rounded-full hover:bg-black hover:text-white">
                         4 more+
                     </button>
                 </div>
